4c644091fa07b102c9b21bd5f9ae840ecfce9f3f
Jon <jon.hogan83@gmail.com>
2021-10-24 21:08:30 -0500

    Finish IC2

--- assignments/inclass/oct22/inClass2.c
+++ assignments/inclass/oct22/inClass2.c
@@ -1,41 +1,44 @@
+//Jonathan Hogan
+//mpicc -g -Wall -o inClass2 inClass2.c
+//sbatch slurm.sh
+
+
 #include <stdio.h>
 #include <mpi.h>
 
-#define SIZE 640000
+#define SIZE 10000
 
 int main(void)
 {
     int comm_sz; //number of ranks
     int my_rank; //processes
-    /*long long A[SIZE];
-
-    for (int i = 0; i<SIZE;i++){
-        A[i] = i+1;
-    }*/
+    
 
     MPI_Init(NULL, NULL);
 
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    long long local=0;
-    long long globalSum = 0;
 
-    long long A[SIZE];
+    long long arr[SIZE];
 
-    for (int i = 0; i<SIZE;i++){
-        A[i] = i+1;
-    }    
-
-    for (int i = my_rank*10000; i<(my_rank*10000)+10000;i++){
-        local+=A[i];
+    for (long i = 0; i<SIZE;i++){
+        arr[i]=(SIZE * my_rank)+i+1;
     }
 
-    MPI_Reduce(&local,&globalSum,1,MPI_LONG_LONG,MPI_SUM,0,MPI_COMM_WORLD);
-
-    if (my_rank==0){
-        printf("The summation of all numbers is: %lld",globalSum);
+    if (my_rank == 0){
+        long long total[SIZE] = {0};
+        MPI_Reduce(&arr,&total,SIZE,MPI_LONG_LONG,MPI_SUM,0,MPI_COMM_WORLD);
+        long long sum = 0;
+        
+        for (long i = 0; i < SIZE; i++){sum += total[i];}
+        
+        printf("The summation of all numbers is: %lld",sum);
+        
+    }else{
+      MPI_Reduce(&arr,NULL,SIZE,MPI_LONG_LONG,MPI_SUM,0,MPI_COMM_WORLD);
     }
+
 
     MPI_Finalize();
 }
