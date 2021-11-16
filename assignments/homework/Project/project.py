0f575219d8d552f5f17034f982e887e506ee1b30
Jon <18120090+jonhogan@users.noreply.github.com>
2021-11-16 16:58:06 -0600

    Update

--- assignments/homework/Project/project.py
+++ assignments/homework/Project/project.py
@@ -1,15 +1,6 @@
-class Word:
+class LetterNode:
+    letter = ''
+    count = 0
     def __ini__(self, text, firstword = False, endword = False ):
         self.word = text
-
-class TrieNode:
-    def __init__(self, word = '', increment = 0):
-        self.word = [word.word]#Not sure this is right
-        self.word.append(word)
-        self.child = dict()
-        self.count += increment
-
-class TrieTree:
-    def __init__(self):
-        self.root = TrieNode()
-
+        letter = self.word[0]
