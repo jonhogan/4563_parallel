1dc66a59d85b9a68dce39663a341174b1b5fc811
Jon <jon.hogan83@gmail.com>
2021-11-17 19:45:54 -0600

    Starting the Markov Chain

--- assignments/homework/Project/project.py
+++ assignments/homework/Project/project.py
@@ -1,6 +1,18 @@
-class LetterNode:
-    letter = ''
-    count = 0
-    def __ini__(self, text, firstword = False, endword = False ):
-        self.word = text
-        letter = self.word[0]
+import numpy as np
+
+#Markov chain
+
+lexiWord = {}
+
+def update_lexiWord(current : str, next : str)-> None:
+    
+    #Add new words to the words lexicon
+    if current not in lexiWord:
+        lexiWord.update({current:{next: 1}})
+        return
+
+    #Get the probabilities of the input word
+    options = lexiWord[current]
+
+    
+    
