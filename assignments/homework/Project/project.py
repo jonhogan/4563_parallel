0d0691a80693ea65bb8c295452ce0fb525a12020
Jon Hogan <jon.hogan83@gmail.com>
2021-11-17 21:52:31 -0600

    Markov Chain

--- assignments/homework/Project/project.py
+++ assignments/homework/Project/project.py
@@ -1,21 +1,52 @@
 import numpy as np
 
-#Markov chain
+# Markov chain stored as adjacency list.
+lexicon = {}
 
-lexiWord = {}
+def update_lexicon(current : str, next_word : str) -> None:
+    """Add item to the lexicon.
 
-def update_lexiWord(current : str, next : str)-> None:
-    
-    #Add new words to the words lexicon
-    if current not in lexiWord:
-        lexiWord.update({current:{next: 1}})
+    Args:
+        current (str): Input word.
+        next_word (str): Output word.
+    """
+
+    # Add the input word to the lexicon if it in there yet.
+    if current not in lexicon:
+        lexicon.update({current: {next_word: 1} })
         return
 
-    #Get the probabilities of the input word
-    options = lexiWord[current]
-<<<<<<< HEAD
+    # Recieve te probabilties of the input word.
+    options = lexicon[current]
 
-    
-    
-=======
->>>>>>> ede60d50d1431f0a8ceb76b3a6ffc9def39c4b23
+    # Check if the output word is in the propability list.
+    if next_word not in options:
+        options.update({next_word : 1})
+    else:
+        options.update({next_word : options[next_word] + 1})
+
+    # Update the lexicon
+    lexicon[current] = options
+
+# Populate lexicon
+with open('dataset.txt', 'r') as dataset:
+    for line in dataset:
+        words = line.strip().split(' ')
+        for i in range(len(words) - 1):
+            update_lexicon(words[i], words[i+1])
+        
+# Adjust propability
+for word, transition in lexicon.items():
+    transition = dict((key, value / sum(transition.values())) for key, value in transition.items())
+    lexicon[word] = transition
+
+# Predict next word
+line = input('> ')
+word = line.strip().split(' ')[-1]
+if word not in lexicon:
+    print('Word not found')
+else:
+    options = lexicon[word]
+    predicted = np.random.choice(list(options.keys()), p=list(options.values()))
+    print(line + ' ' + predicted)
+          
